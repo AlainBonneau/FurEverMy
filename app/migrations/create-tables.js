e59216e02ee925bc420f711cc9fcfed8f8282ef8
Alain Bonneau <alaindev97@gmail.com>
2025-01-28 02:28:47 +0100

    change table reset

--- app/migrations/create-tables.js
+++ app/migrations/create-tables.js
@@ -1,7 +1,26 @@
 import { sequelize } from "../models/associations.js";
 
-console.log("🗑️ Suppression des tables existantes...");
-await sequelize.drop();
+async function dropTables() {
+  try {
+    console.log("🗑️ Suppression des tables existantes...");
+    await sequelize
+      .getQueryInterface()
+      .dropTable("animal_has_task", { cascade: true });
+    await sequelize
+      .getQueryInterface()
+      .dropTable("user_has_task", { cascade: true });
+    await sequelize
+      .getQueryInterface()
+      .dropTable("animal_has_breed", { cascade: true });
+    await sequelize.getQueryInterface().dropTable("species", { cascade: true });
+    await sequelize.getQueryInterface().dropTable("breed", { cascade: true });
+    console.log("✅ Toutes les tables ont été supprimées.");
+  } catch (error) {
+    console.error("❌ Erreur lors de la suppression des tables :", error);
+  }
+}
+
+dropTables();
 
 console.log("🚧 Définition des tables...");
 await sequelize.sync();
